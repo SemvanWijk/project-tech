3faf2e8ce9a94565764034ce862d4228fb7a6077
SemvanWijk <120381973+SemvanWijk@users.noreply.github.com>
2024-03-01 10:22:01 +0100

    login

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hallo hallo")
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    var data = {
+        username: username,
+        password: password
+    };
+
+    fetch('http://localhost:3001/login', { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then(data => {
+        console.log(data); 
+    })
+    .catch(error => {
+        console.error('There was a problem with your fetch operation:', error);
+    });
+});
